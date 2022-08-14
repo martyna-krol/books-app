e3d5b72a34ad27da047110cfddb0b07821eb90ac
Martyna Król <martyna.maria.krol@gmail.com>
2022-08-14 18:50:25 +0200

    Redux summary - book app function

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import BookForm from "./components/BookForm/BookForm";
 import BookList from "./components/BooksList/BooksList";
-import { useState } from "react";
-import shortid from "shortid";
 
 const App = () => {
-
-  const [books, setBooks] = useState([
-    { id: 1, title: 'of Mice and Men', author: 'John Steinbeck'},
-    { id: 2, title: 'the Witcher', author: 'Andrzej Sapkowski'}
-  ]);
-
-  const removeBook = bookId => {
-    setBooks(books.filter(book => book.id !== bookId))
-  }
-
-  const addBook = newBook => {
-    setBooks([...books, {id: shortid(), title: newBook.title, author: newBook.author}]);
-  }
 
   return (
     <div className="App">
       <h1>Books App</h1>
-      <BookList books={books} removeBook={removeBook} />
-      <BookForm addBook={addBook}/>
+      <BookList />
+      <BookForm />
     </div>
   );
 }
--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,4 +1,15 @@
-const BookList = ({ books, removeBook }) => {
+import { useSelector } from "react-redux/es/exports";
+import { useDispatch } from "react-redux/es/hooks/useDispatch";
+
+const BookList = () => {
+
+    const books = useSelector(state => state.books)
+    const dispatch = useDispatch();
+
+    const removeBook = bookId => {
+        dispatch({ type: 'REMOVE_BOOK', payload: bookId})
+    }
+
     return (
         <ul>
             {books.map(book => <li key={book.id}>{book.title} by {book.author} <button onClick={() => removeBook(book.id)}>Remove</button></li>)}
